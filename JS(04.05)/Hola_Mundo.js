c6d00f4ea21841e0573200f602eafd6928aee5f3
NeloGino <nelogino@gmail.com>
2017-05-04 09:48:30 -0300

    Primer ejercicio JS

---
+++ JS(04.05)/Hola_Mundo.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Alumno on 04/05/2017.
+ */
+
+/**
+ * Sumatoria de dos numeros
+ * @method Calcular
+ * @param num 1
+ * @param num 2
+ * @return total
+ */
+
+function calcular(num1, num2) {
+    var total;
+    total = num1 + num2;
+    return total;
+}
